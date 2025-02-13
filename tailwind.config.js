2291218020457f934387b6523eaaac9fd4fa047b
TegaHilton <tegahilton@gmail.com>
2025-02-13 20:24:42 +0100

    nav gap

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -13,7 +10,13 @@ export default {
         brightPink: "#BD83B8",
         warmOrange: "#F1916D",
       },
+      spacing: {
+        128: "32rem", // You can add custom values here
+        144: "36rem", // For example, gap-128 and gap-144
+        160: "40rem",
+        150: "50rem", // And so on
+      },
     },
   },
   plugins: [],
-}
+};
